f20e50c7d43301f7540d0c7a130049aa394cfbe7
Carrol Cox <carrol@protonmail.com>
2020-08-22 21:57:27 +0500

    [upd] diagrams
    #DOC

--- diagrams/main.py
+++ diagrams/main.py
@@ -1,15 +1,14 @@
-from diagrams import Cluster, Diagram
+from diagrams import Cluster, Diagram, Edge
 from diagrams.onprem.iac import Ansible
 from diagrams.onprem.monitoring import Grafana, Prometheus
 from diagrams.onprem.queue import RabbitMQ
 from diagrams.programming.framework import Django
 from diagrams.programming.language import Python
 
-with Diagram("MQ Cluster", show=False):
+with Diagram("MQ Cluster", show=False, direction="TB"):
     provisioner = Ansible("mgmt-core")
 
-    metrics = Prometheus("rabbitmq_exporter")
-    metrics << Grafana("monitoring")
+    metrics = Grafana("monitoring") >> Edge(color="firebrick", style="dashed") >> Prometheus("rabbitmq_exporter")
 
     producer = Django("Django App")
     consumer = Python("Celery worker")
@@ -20,22 +19,21 @@ with Diagram("MQ Cluster", show=False):
                         RabbitMQ("follower_2")]
 
     provisioner >> mq_cluster
-    producer >> mq_cluster >> consumer
-    mq_cluster >> metrics
+    producer >> Edge(color="brown") >> mq_cluster << Edge(color="brown") << consumer
+    mq_cluster <<  metrics
 
 
-with Diagram("MQ Single", show=False):
+with Diagram("MQ Single", show=False, direction="TB"):
     provisioner = Ansible("mgmt-core")
 
-    metrics = Prometheus("rabbitmq_exporter")
-    metrics << Grafana("monitoring")
+    metrics = Grafana("monitoring") >> Edge(color="firebrick", style="dashed") >> Prometheus("rabbitmq_exporter")
 
     producer = Django("Django App")
     consumer = Python("Celery worker")
 
     with Cluster("RabbitMQ Single Node Cluster"):
-        mq_cluster = RabbitMQ("leader")
+        mq_cluster = [RabbitMQ("leader")]
 
     provisioner >> mq_cluster
-    producer >> mq_cluster >> consumer
-    mq_cluster >> metrics
+    producer >> Edge(color="brown") >> mq_cluster << Edge(color="brown") << consumer
+    mq_cluster <<  metrics
